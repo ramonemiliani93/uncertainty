f806a7e0357e5410db246d85d2693caeaa338880
milongo <emilio.botero@umontreal.ca>
2019-11-29 11:18:57 -0500

    switch to ignite

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import argparse
 import os
-import sys
+
 import importlib
 import torch
 import torch.nn
-
+from torch.utils.data import DataLoader
 import logging
-import tqdm
 import utils
 from tensorboardX import SummaryWriter
+from ignite.engine import Events, create_supervised_trainer, create_supervised_evaluator
+from ignite.metrics import Loss
+from ignite.utils import convert_tensor
+from ignite.engine.engine import Engine, State, Events
 
-from data_loader.datasets import SineDataset
-from mixin.montecarlo import MonteCarloMixin
+
+def create_summary_writer(log_dir):
+    writer = SummaryWriter(logdir=log_dir)
+    return writer
 
 
 def instantiate(module, name):
@@ -20,61 +25,83 @@ def instantiate(module, name):
     return instance
 
 
-def train(model, optimizer, dataloader, params, metrics):
-    """Train the model on `num_steps` batches
-    Args:
-        model: (torch.nn.Module) the neural network
-        optimizer: (torch.optim) optimizer for parameters of model
-        loss_fn: a function that takes batch_output and batch_labels and computes the loss for the batch
-        dataloader: (DataLoader) a torch.utils.data.DataLoader object that fetches training data
-        metrics: (dict) a dictionary of functions that compute a metric using the output and labels of each batch
-        params: (Params) hyperparameters
-        num_steps: (int) number of batches to train on, each of size params.batch_size
+# TODO
+def get_data_loaders(train_batch_size, val_batch_size):
+    train_loader = DataLoader()
+    val_loader = DataLoader()
+    return train_loader, val_loader
+
+
+def _prepare_batch(batch, device=None, non_blocking=False):
+    """Prepare batch for training: pass to a device with options.
     """
+    x, y = batch
+    return (convert_tensor(x, device=device, non_blocking=non_blocking),
+            convert_tensor(y, device=device, non_blocking=non_blocking))
 
-    # set model to training mode
-    model.train()
 
-    # Metrics?
+def create_train_engine(model, optimizer,
+                        device=None, non_blocking=False,
+                        prepare_batch=_prepare_batch,
+                        output_transform=lambda x, y, y_pred, loss: loss.item()):
+    if device:
+        model.to(device)
 
-    # Use tqdm for progress bar
-    with tqdm(total=len(dataloader)) as t:
-        for i, data in enumerate(dataloader):
-            # move to GPU if available
-            if torch.cuda.is_available():
-                train_batch = train_batch.to(params.device)
-                labels_batch = labels_batch.to(params.device)
+    def _update(engine, batch):
+        model.train()
+        optimizer.zero_grad()
+        x, y = prepare_batch(batch, device=device, non_blocking=non_blocking)
+        y_pred = model(x)
+        loss = model.loss(*(x, y))  # TODO CHECK
+        loss.backward()
+        optimizer.step()
+        return output_transform(x, y, y_pred, loss)
 
-            loss = model.loss(*data)
+    return Engine(_update)
 
-            # update the average loss
-            metrics['loss'].update(loss.item())
 
-            # clear previous gradients, compute gradients of all variables wrt loss
-            optimizer.zero_grad()
-            loss.backward()
+def run(model, loss, optimizer, train_batch_size, val_batch_size, epochs, log_interval, log_dir):
+    train_loader, val_loader = get_data_loaders(train_batch_size, val_batch_size)
+    writer = create_summary_writer(log_dir)
+    device = 'cpu'
 
-            # performs updates using calculated gradients
-            optimizer.step()
+    if torch.cuda.is_available():
+        device = 'cuda'
 
-            # Evaluate summaries only once in a while
-            if i % params.save_summary_steps == 0:
-                # extract data from torch Variable, move to cpu, convert to numpy arrays
-                output_batch = output_batch.data.cpu().numpy()
-                labels_batch = labels_batch.data.cpu().numpy()
+    # trainer = create_supervised_trainer(model, optimizer, loss, device=device)
+    trainer = create_train_engine(model, optimizer, device=device)
+    evaluator = create_supervised_evaluator(model,
+                                            metrics={'mse': Loss(loss)},
+                                            device=device)
 
-                # compute all metrics on this batch
-                for metric in metrics.values():
-                    metric(output_batch, labels_batch)
+    @trainer.on(Events.ITERATION_COMPLETED(every=log_interval))
+    def log_training_loss(engine):
+        print("Epoch[{}] Iteration[{}/{}] Loss: {:.2f}"
+              "".format(engine.state.epoch, engine.state.iteration, len(train_loader), engine.state.output))
+        writer.add_scalar("training/loss", engine.state.output, engine.state.iteration)
 
-            t.set_postfix(loss='{:05.3f}'.format(metrics['loss'].mean()))
-            t.update()
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def log_training_results(engine):
+        evaluator.run(train_loader)
+        metrics = evaluator.state.metrics
+        avg_mse = metrics['mse']
+        print("Training Results - Epoch: {}   Avg loss: {:.2f}"
+              .format(engine.state.epoch, avg_mse))
+        writer.add_scalar("training/avg_loss", avg_mse, engine.state.epoch)
 
-    # Summary of metrics in log
-    metrics_string = "".join([metric.__str__(y_labels=params.y_labels) for metric in metrics.values()])
-    logging.info("- Train metrics: \n" + metrics_string)
+    @trainer.on(Events.EPOCH_COMPLETED)
+    def log_validation_results(engine):
+        evaluator.run(val_loader)
+        metrics = evaluator.state.metrics
+        avg_mse = metrics['mse']
+        print("Validation Results - Epoch: {}  Avg loss: {:.2f}"
+              .format(engine.state.epoch, avg_mse))
+        writer.add_scalar("valdation/avg_loss", avg_mse, engine.state.epoch)
 
-    return metrics
+    # kick everything off
+    trainer.run(train_loader, max_epochs=epochs)
+
+    writer.close()
 
 
 if __name__ == '__main__':
@@ -86,24 +113,23 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     yaml_path = os.path.join(args.model_dir, 'params.yml')
-    assert os.path.isfile(yaml_path), "No json configuration file found at {}".format(yaml_path)
+    assert os.path.isfile(yaml_path), "No yaml configuration file found at {}".format(yaml_path)
     params = utils.Params(yaml_path)
-    # Tensorboard writer
-    # writer = SummaryWriter(args.model_dir)
 
     # Instantiate model
     model_module, model_name = params.model['module'], params.model['name']
-    model = instantiate(model_module, model_name)
+    model_to_train = instantiate(model_module, model_name)
 
     # Instantiate optimizer
     optimizer_module, optimizer_name = params.optimizer['module'], params.optimizer['name']
     optimizer = instantiate(optimizer_module, optimizer_name)
 
+    # Instantiate loss
+    loss_module, loss_name = params.optimizer['module'], params.optimizer['name']
+    loss_fn = instantiate(optimizer_module, optimizer_name)
+
+    # Instantiate dataset
+    dataset_module, dataset_name = params.dataset['module'], params.dataset['name']
+
     # Train the model
     logging.info("Starting training for {} epoch(s)".format(params.num_epochs))
-    # train(model, train_dl, val_dl, optimizer, metrics, params, args.model_dir,
-    #                   args.restore_file, writer)
-
-    # writer.export_scalars_to_json(os.path.join(args.model_dir, "all_scalars.json"))
-    # writer.close()
-
