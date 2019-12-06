ad372cd99ebf2308f2b303ad8f7dfe8377a3d5c9
Lawrence Abdulnour <Lawrenceee94@gmail.com>
2019-12-05 19:16:45 -0500

    Add UCI concrete dataset

---
+++ dataloader/datasets/concrete.py
@@ -0,0 +1,45 @@
+import torch
+import numpy as np
+import pandas as pd
+from typing import Tuple
+from torch.utils.data import Dataset
+
+
+class ConcreteDataset(Dataset):
+    def __init__(self):
+        super(ConcreteDataset, self).__init__()
+
+        concrete = pd.read_excel(r"../data/concrete_data.xls")
+        self.features = concrete.values[:, :-1]
+        self.targets = concrete.values[:, -1:]
+
+        assert len(self.features) == len(self.targets)
+
+    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
+        sample = torch.tensor(self.features[idx])
+        target = torch.tensor(self.targets[idx])
+
+        return sample, target
+
+    def __len__(self):
+        return len(self.features)
+
+    def generate_neighbors(self, neighbors, **kwargs):
+        # Extract parameters if provided in kwargs.
+        dimension = 1  # Length of item vector that will be indexed
+        metric = kwargs.get('metric', 'euclidean')
+        num_trees = kwargs.get('num_trees', 10)
+
+        # Build tree with the given data.
+        t = AnnoyIndex(dimension, metric)
+        for i in range(self.num_samples):
+            t.add_item(i, [self.samples[i].item()])
+        t.build(num_trees)
+
+        # Generate neighbor map array.
+        neighbor_map = np.zeros((self.num_samples, neighbors))
+        for i in range(self.num_samples):
+            nearest_neighbors = t.get_nns_by_item(i, neighbors)
+            neighbor_map[i, :] = nearest_neighbors
+
+        return neighbor_map.astype(int)
