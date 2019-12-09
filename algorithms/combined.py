c5c363f17f941c688dd74f537aaf5dbcfedbf42d
Ramon Emiliani <ramon.emiliani@umontreal.ca>
2019-12-09 15:06:51 -0500

    Implement using t location-scaled NLL (up to 3.4 in the paper).

--- algorithms/combined.py
+++ algorithms/combined.py
@@ -1,5 +1,6 @@
 from typing import Tuple
 
+from sklearn.cluster import KMeans
 import torch
 from torch import nn
 from torch.nn.functional import softplus
@@ -17,19 +18,25 @@ class Combined(UncertaintyAlgorithm):
         self.init_gamma: int = 'init_gamma'
         self.inducing_points: float = 'inducing_points'
         self.eta: float = 'eta'
+        self.switch_modulo: int = 'switch_modulo'
+        self.warm_start_it: int = 'warm_start_it'
 
         # Create models and register parameter
         model = kwargs.get('model')
         self.mean = model(**kwargs)
         self.alpha = model(**kwargs)
         self.beta = model(**kwargs)
-        self.gamma = nn.Parameter(torch.tensor(self.init_gamma))
+        # self.gamma = nn.Parameter(torch.tensor(self.init_gamma))
         self.model = nn.ModuleDict({
             'mean': self.mean,
             'alpha': self.alpha,
             'beta': self.beta,
-            'gamma': self.gamma
+            # 'gamma': self.gamma
         })
+
+        # Extract dataset and use inducing points to generate clusters
+        # dataset = kwargs.get('dataset')
+        # _ = [dataset[index][0] for index in range(len(dataset))]
 
         # Reserved params
         self._current_it = 0
@@ -39,7 +46,7 @@ class Combined(UncertaintyAlgorithm):
         data, target, probability = args
 
         # Mean-variance split training
-        if self._current_it % 2:
+        if self._current_it % self.switch_modulo or self._current_it < self.warm_start_it:
             mean = self.mean(data)
             with torch.no_grad():
                 alpha = softplus(self.alpha(data))
@@ -50,48 +57,28 @@ class Combined(UncertaintyAlgorithm):
             alpha = softplus(self.alpha(data))
             beta = softplus(self.beta(data))
 
+        nll = self.calculate_nll_student_t(target, mean, alpha, beta)
+        weighted_nll = (nll * probability).mean()
 
-        return nll
+        # Update current iteration
+        self._current_it += 1
+
+        return weighted_nll
 
     def predict_with_uncertainty(self, *args, **kwargs) -> Tuple[torch.Tensor, torch.Tensor]:
         # Set model to evaluation
         self.mean.eval()
-        self.log_variance.eval()
+        self.alpha.eval()
+        self.beta.eval()
 
         # Sample multiple times from the ensemble of models
-        predictive_mean_list, predictive_log_variance_list = [], []
         with torch.no_grad():
-            for i in range(self.num_models):
-                predictive_mean = self.mean[i](args[0])
-                predictive_mean_list.append(predictive_mean)
-                predictive_log_variance = self.log_variance[i](args[0])
-                predictive_log_variance_list.append(predictive_log_variance)
+            mean = self.mean(args[0])
+            alpha = self.alpha(args[0])
+            beta = self.alpha(args[0])
+            variance = beta / alpha
 
-            # Stack each of the models
-            predictive_mean_ensemble = torch.stack(predictive_mean_list)
-            predictive_log_variance_ensemble = torch.stack(predictive_log_variance_list)
-
-            # Compute statistics
-            predictive_mean_model = predictive_mean_ensemble.mean(0)
-            # print(predictive_log_variance)
-            predictive_variance_model = (torch.exp(predictive_log_variance_ensemble)
-                                         + predictive_mean_ensemble ** 2).mean(0) - predictive_mean_model ** 2
-            preditive_std_model = predictive_variance_model.sqrt()
-
-        return predictive_mean_model, preditive_std_model
-
-    def fgsm_attack(self, data, data_grad):
-        # Collect the element-wise sign of the data gradient
-        sign_data_grad = data_grad.sign()
-
-        # Find max value of the data
-        max_value = data.max()
-
-        # Create the perturbed image by adjusting each pixel of the input image
-        perturbed_data = data + self.eps * max_value * sign_data_grad
-
-        # Return the perturbed image
-        return perturbed_data
+        return mean, variance
 
     @staticmethod
     def calculate_nll_student_t(target: torch.Tensor, mean: torch.Tensor, alpha: torch.Tensor, beta: torch.Tensor):
@@ -113,9 +100,15 @@ if __name__ == '__main__':
 
     from data_loader.datasets import SineDataset
     from models.mlp import MLP
+    params = {'init_gamma': 1.5,
+              'inducing_points': 500,
+              'eta': 3.5 ** 2,
+              'switch_modulo': 2,
+              'model': MLP,
+              'warm_start_it': 0
+              }
 
-    params = {'num_models': 1, 'eps': 0.01, 'adversarial': False, 'model': MLP, 'warm_start_it': 15000}
-    algorithm = DeepEnsembles(**params)
+    algorithm = Combined(**params)
     kwargs = {'num_samples': 500, 'domain': (0, 10)}
     train_loader = DataLoader(SineDataset(**kwargs), batch_size=500)
     optimizer = Adam(algorithm.model.parameters(), lr=1e-2, weight_decay=0)
@@ -128,7 +121,7 @@ if __name__ == '__main__':
             optimizer.zero_grad()
 
             # forward + backward + optimize
-            loss = algorithm.loss(*data, warm_start_it=50000, it=epoch)
+            loss = algorithm.loss(*data, it=epoch)
             loss.backward()
             optimizer.step()
 
@@ -142,5 +135,5 @@ if __name__ == '__main__':
     x = np.linspace(-4, 14, 5000)
     x_tensor = torch.FloatTensor(x).reshape(-1, 1)
     mean, var = algorithm.predict_with_uncertainty(x_tensor)
-    std = np.sqrt(var)
-    plot_toy_uncertainty(x, mean, std, train_loader)
+    std = np.sqrt(var.squeeze())
+    plot_toy_uncertainty(x, mean.squeeze(), std, train_loader)
