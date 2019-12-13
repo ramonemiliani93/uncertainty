981e47add7bc905326dd176bbfb46e9f9325c746
milongo <emilio.botero@umontreal.ca>
2019-12-13 09:43:39 -0500

    BNN works with 1 hidden layer

--- algorithms/bnn.py
+++ algorithms/bnn.py
@@ -38,49 +38,55 @@ class BNN(UncertaintyAlgorithm):
         self.args = kwargs
         self.scaler = preprocessing.StandardScaler()
 
-    def model(self, x_train, y_train, d_h):
-        d_x = x_train.shape[1]
-        d_y = x_train.shape[1]
+    def model(self, x_train, y_train, num_hidden):
+        d_x = x_train.shape[1]  # number of features in x
+        d_y = y_train.shape[1]  #
         # d_x, d_y = x_train.shape[1], 1
 
         # sample first layer (we put unit normal priors on all weights)
-        w1 = numpyro.sample("w1", dist.Normal(np.zeros((d_x, d_h)), np.ones((d_x, d_h))))  # d_x d_h
-        b1 = numpyro.sample("b1", dist.Normal(0, 1), sample_shape=(d_h,))
-        z1 = b1 + nonlin(np.matmul(x_train, w1))
+        w1 = numpyro.sample("w1", dist.Normal(np.zeros((d_x, num_hidden)), np.ones((d_x, num_hidden))))  # d_x d_h
+        b1 = numpyro.sample("b1", dist.Normal(0, 1), sample_shape=(num_hidden,))
+        z1 = nonlin(b1 + np.matmul(x_train, w1))
         # z1 = nonlin(np.matmul(x_train, w1))   # N d_h  <= first layer of activations
 
         # sample second layer
-        w2 = numpyro.sample("w2", dist.Normal(np.zeros((d_h, d_h)), np.ones((d_h, d_h))))  # d_h d_h
-        b2 = numpyro.sample("b2", dist.Normal(0, 1), sample_shape=(d_h,))
-        z2 = b2 + nonlin(np.matmul(z1, w2))  # N d_h  <= second layer of activations
+        w2 = numpyro.sample("w2", dist.Normal(np.zeros((num_hidden, d_y)), np.ones((num_hidden, d_y))))  # d_h d_h
+        b2 = numpyro.sample("b2", dist.Normal(0, 1), sample_shape=(d_y,))
+        z2 = b2 + np.matmul(z1, w2)  # N d_h  <= second layer of activations
 
         # sample final layer of weights and neural network output
-        w3 = numpyro.sample("w3", dist.Normal(np.zeros((d_h, d_y)), np.ones((d_h, d_y))))  # d_h D_Y
-        z3 = np.matmul(z2, w3)  # N D_Y  <= output of the neural network
+        # w3 = numpyro.sample("w3", dist.Normal(np.zeros((d_h, d_y)), np.ones((d_h, d_y))))  # d_h D_Y
+        # z3 = np.matmul(z2, w3)  # N D_Y  <= output of the neural network
 
         # we put a prior on the observation noise
         prec_obs = numpyro.sample("prec_obs", dist.Gamma(3.0, 1.0))
         sigma_obs = 1.0 / np.sqrt(prec_obs)
 
         # observe data
-        numpyro.sample("y_train", dist.Normal(z3, sigma_obs), obs=y_train)
+        numpyro.sample("y_train", dist.Normal(z2, sigma_obs), obs=y_train)
 
     def main(self):
-        N, d_x, d_h = self.args['num_data'], 1, self.args['num_hidden']
+
         x_train, y_train = self.dataset.samples, self.dataset.targets
         x_test = np.linspace(-4, 14, 5000)
-        x_train = np.expand_dims(x_train, -1)
-        y_train = np.expand_dims(y_train, -1)
         x_test = np.expand_dims(x_test, -1)
 
+        if len(x_train.shape) == 1:
+            x_train = np.expand_dims(x_train, -1)
+        if len(y_train.shape) == 1:
+            y_train = np.expand_dims(y_train, -1)
+
+        num_hidden = self.args['num_hidden']
+        # rescale y
         y_train = self.scaler.fit_transform(y_train)
+
         # do inference
         rng_key, rng_key_predict = random.split(random.PRNGKey(0))
-        samples = self.run_inference(self.args, rng_key, x_train, y_train, d_h)
+        samples = self.run_inference(self.args, rng_key, x_train, y_train, num_hidden)
 
         # predict Y_test at inputs x_test
         vmap_args = (samples, random.split(rng_key_predict, self.args['num_samples'] * self.args['num_chains']))
-        predictions = vmap(lambda samples, rng_key: self.predict(self.model, rng_key, samples, x_test, d_h))(*vmap_args)
+        predictions = vmap(lambda samples, rng_key: self.predict(self.model, rng_key, samples, x_test, num_hidden))(*vmap_args)
         predictions = predictions[..., 0]
 
         # compute mean prediction and confidence interval around median
@@ -88,15 +94,11 @@ class BNN(UncertaintyAlgorithm):
         var_prediction = np.var(predictions, axis=0)
         std_prediction = np.sqrt(var_prediction)
 
-        # plot training data
-        # plot mean prediction
+        # rescale
         mean_prediction *= self.scaler.scale_
         mean_prediction += self.scaler.mean_
 
-        y_train *= self.scaler.scale_
-        y_train += self.scaler.mean_
-
-        return mean_prediction, x_train, y_train, x_test, std_prediction
+        return mean_prediction, x_test, std_prediction
 
     def predict(self, model, rng_key, samples, x_train, d_h):
         model = handlers.substitute(handlers.seed(model, rng_key), samples)
@@ -120,8 +122,9 @@ if __name__ == '__main__':
                   'num_data': 500,
                   'num_samples': 2000,
                   'num_warmup': 1000,
-                  'num_hidden': 3,
+                  'num_hidden': 50,
                   'device': 'cpu'}
+
     # dataset params
     dataset_params = kwargs = {'num_samples': 500, 'domain': (0, 10)}
     dataset = SineDataset(**kwargs)
@@ -131,10 +134,10 @@ if __name__ == '__main__':
 
     train_loader = DataLoader(dataset, batch_size=500)
     algorithm = BNN(**bnn_params)
-    mean_prediction, x_train, y_train, x_test, std_prediction = algorithm.main()
-    x_test = np.squeeze(x_test)
-    mean = torch.from_numpy(onp.array(mean_prediction)).squeeze()
-    std = torch.from_numpy(onp.array(std_prediction)).squeeze()
 
+    mean, x_test, std = algorithm.main()
+
+    x_test = np.squeeze(x_test)
+    mean = torch.from_numpy(onp.array(mean)).squeeze()
+    std = torch.from_numpy(onp.array(std)).squeeze()
     plot_toy_uncertainty(x_test, mean, std, train_loader)
-    x = 1
